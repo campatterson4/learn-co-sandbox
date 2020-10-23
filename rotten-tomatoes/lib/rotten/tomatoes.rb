834efe40c9e8e39f8b0fbc2cae335def8f5a3902
Camryn Patterson <camrynpatterson@yahoo.com>
2020-10-23 21:35:15 +0000

    Automatically backed up by Learn

---
+++ rotten-tomatoes/lib/rotten/tomatoes.rb
@@ -0,0 +1,8 @@
+require "rotten/tomatoes/version"
+
+module Rotten
+  module Tomatoes
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ rotten-tomatoes/spec/rotten/tomatoes_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rotten::Tomatoes do
+  it "has a version number" do
+    expect(Rotten::Tomatoes::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
